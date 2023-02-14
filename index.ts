107e88affe6ecad39ac0472ab1578c135f2e44bb
Maxis <maxdopp.md@gmail.com>
2023-02-14 21:43:09 +0100

    removed unimplemented SImage

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
-import { Selection } from "./src/selection";
+// import { Selection } from "./src/selection"; //? No usecase implemented yet
 import { Matrix } from "./src/matrix";
-import { SImage } from "./src/simage";
+// import { SImage } from "./src/simage" //? Not implemented yet
 import { Canvas } from "./src/canvas";
 import { Brush } from "./src/brush";
 import { Point } from "./src/point";
 import { Path } from "./src/path";
 
-export { Selection, Canvas, Brush, SImage, Path, Matrix, Point };
+export {
+  // Selection,
+  Canvas,
+  Brush,
+  //SImage,
+  Path,
+  Matrix,
+  Point,
+};
